409f6f1a0b0dc759fc47e9e2777c0be0852d864c
Elijah-karori <blakkarori@gmail.com>
2022-10-06 19:36:16 +0300

    Update signin.js

--- routes/auth/signin.js
+++ routes/auth/signin.js
@@ -12,7 +12,7 @@ router.get('/', async (req,res)=>{
 router.post('/',async (req,res,next)=>{
     console.log(req.body.email)
     const userdoc =await user.findOne({'email': req.body.email})
-     if((userdoc.email != req.body.email&& (userdoc.email!=null) || req.body.password === req.body.password2){ 
+     if((userdoc.email != req.body.email&& (userdoc.email!=null)) || req.body.password === req.body.password2){ 
             try {
                 const password= await bcrypt.hash(req.body.password, saltRounds);
                 const User = new user(
